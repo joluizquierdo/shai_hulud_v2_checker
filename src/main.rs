311c2d50e0b2536c6423b3c1dc83fa7e02a64e16
Jolu Izquierdo <22649229+joluizquierdo@users.noreply.github.com>
2025-11-27 13:48:32 +0100

    feat: read package-lock.json

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::{fs, path::Path, process};
+const JSON_LOCK_FILE: &str = "package-lock.json";
+
 fn main() {
-    println!("Hello, world!");
+    //TODO: read JSON from CLI arg or default to JSON_LOCK_FILE
+    let path = Path::new(JSON_LOCK_FILE);
+    if !path.exists() || !path.is_file() {
+        eprintln!(
+            "File '{}' doesn't exists or is not a valid file aborting",
+            path.to_string_lossy()
+        );
+        process::exit(1);
+    }
+    println!(
+        "File '{}' found, proceeding with the scan...",
+        path.to_string_lossy()
+    );
+
+    // let json_str = fs::read_to_string();
 }
