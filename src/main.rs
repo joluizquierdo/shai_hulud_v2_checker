59f3ccde2cf9385f3c4473fdc27536e94fc807e5
Jolu Izquierdo <22649229+joluizquierdo@users.noreply.github.com>
2025-11-27 14:32:01 +0100

    feat: add npm installation check

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,33 @@
 mod models;
 
 use models::json_lock::JsonLockPackages;
-use std::{fs, path::Path, process};
+use std::{
+    fs,
+    path::Path,
+    process::{self, Command},
+};
 
 const JSON_LOCK_FILE: &str = "examples/package-lock.json";
 
 fn main() {
+    if !is_npm_installed() {
+        eprintln!("NPM is not installed or not found in PATH. Please install NPM to proceed.");
+        process::exit(1);
+    }
+
     //TODO: read JSON from CLI arg or default to JSON_LOCK_FILE
     let path = Path::new(JSON_LOCK_FILE);
+    let npm_packages = parse_npm_json(path);
+
+    println!(
+        "Packages lock Json processed succesfully! Found {} packages",
+        npm_packages.packages.len()
+    );
+
+    // println!("Parsed NPM Packages: {:#?}", npm_packages);
+}
+
+fn parse_npm_json(path: &Path) -> JsonLockPackages {
     if !path.exists() || !path.is_file() {
         eprintln!(
             "File '{}' doesn't exists or is not a valid file aborting",
@@ -21,13 +41,9 @@ fn main() {
     );
 
     let json_lock_content = fs::read_to_string(path).expect("Failed to read json lock file");
-    let npm_packages: JsonLockPackages =
-        serde_json::from_str(&json_lock_content).expect("Failed to parse JSON");
+    serde_json::from_str(&json_lock_content).expect("Failed to parse JSON")
+}
 
-    println!(
-        "Packages lock Json processed succesfully! Found {} packages",
-        npm_packages.packages.len()
-    );
-
-    // println!("Parsed NPM Packages: {:#?}", npm_packages);
+fn is_npm_installed() -> bool {
+    Command::new("npm").arg("--version").output().is_ok()
 }
