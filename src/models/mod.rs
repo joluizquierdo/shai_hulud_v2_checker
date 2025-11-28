ebe7f891e3c257fe0f4a719e3007d3da7aa9401c
Jolu Izquierdo <22649229+joluizquierdo@users.noreply.github.com>
2025-11-28 18:02:02 +0100

    style: remove space

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -3,6 +3,5 @@
 //! This module contains all data structures used to represent NPM packages,
 //! vulnerability records, and custom deserializers for parsing package-lock.json
 //! and vulnerability CSV files.
-
 pub mod package;
 pub mod serde_helpers;
